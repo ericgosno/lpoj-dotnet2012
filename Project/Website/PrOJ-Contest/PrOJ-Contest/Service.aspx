c3adaf6a706da4d831d8703ba826ee80665398da
felixh.1991@gmail.com <felixh.1991@gmail.com@32ea7ef2-c08a-77b8-362f-daceb00e6559>
2012-06-19 17:48:06 +0000

    Service Jalan (server dan Client)

---
+++ Project/Website/PrOJ-Contest/PrOJ-Contest/Service.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Service.aspx.cs" Inherits="PrOJ_Contest.Service" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContentPlaceHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MenuContentPlaceHolder" runat="server">
+ <ul class="art-hmenu">
+        <li><a href="Default.aspx"><span class="l"></span><span class="r"></span><span class="t">Home</span></a></li>
+        <li><a href="LoginPage.aspx"><span class="l"></span><span class="r"></span><span class="t">Login</span></a></li>
+        <li><a href="Credits.aspx"><span class="l"></span><span class="r"></span><span class="t">Credits</span></a></li>
+        <li><a href="Service.aspx" class="active">><span class="l"></span><span class="r"></span><span class="t">Service</span></a></li>
+    </ul>
+</asp:Content>
+
+<asp:Content ID="Content5" ContentPlaceHolderID="SheetContentPlaceHolder" runat="server">
+<h3>ASPX WebService</h3>
+
+<br />
+<form id="Form1" runat="server">
+ <b>WebService Cek Informasi </b>
+ <br />
+ <asp:TextBox ID = "usernametxt" runat = "server" /> 
+ <br />
+ <asp:Button ID = "btn_service" runat = "server" Text = "Check" 
+     onclick="btn_service_Click"/>
+ <br />
+ <b>Jumlah Kontest yang diikuti </b> <br /> <asp:Label ID = "JumAnswerResp" runat ="server" />
+ <br />
+ <b>Submission Problem </b > <br /> <asp:Label ID = "JumSubmission" runat ="server" />
+</form>
+</asp:Content>
+
---
+++ Project/lpojWebService/lpojWcf/Account.svc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace lpojWcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Account : IAccount
+    {
+        private lpojEntities Entity;
+        /*public Account()
+        {
+            Entity = new lpojEntities();
+        }*/
+        public int NumberContest(string username)
+        {
+            Entity = new lpojEntities();
+            lpoj_users usernow = new lpoj_users();
+            IEnumerable<lpoj_users> query2 = from g in Entity.lpoj_users
+                                             where g.USERS_USERNAME == username
+                                             select g;
+            if (query2.Count() > 0)
+            {
+                usernow = query2.First();
+            }
+            else return -1;
+            IEnumerable<lpoj_contestant> query = from g in Entity.lpoj_contestant
+                                                 where g.CONTESTANT_STATUS == 1 && g.USERS_ID == usernow.USERS_ID
+                                                 select g;
+            return query.Count();
+        }
+        public KeyValuePair<int, int> NumberProblem(string username)
+        {
+            Entity = new lpojEntities();
+            int ACAnswer = 0;
+            int AllAnswer = 0;
+            lpoj_users usernow = new lpoj_users();
+            IEnumerable<lpoj_users> query2 = from g in Entity.lpoj_users
+                                             where g.USERS_USERNAME == username
+                                             select g;
+            if (query2.Count() > 0)
+            {
+                usernow = query2.First();
+            }
+            else return new KeyValuePair<int, int>(-1, -1);
+            IEnumerable<lpoj_contestant> query = from g in Entity.lpoj_contestant
+                                                 where g.CONTESTANT_STATUS == 1 && g.USERS_ID == usernow.USERS_ID
+                                                 select g;
+            for (int i = 0; i < query.Count(); i++)
+            {
+                lpoj_contestant news = query.ElementAt(i);
+                IEnumerable<lpoj_submission> query3 = from g in Entity.lpoj_submission
+                                                      where g.CONTESTANT_ID == news.CONTESTANT_ID
+                                                      select g;
+                for (int j = 0; j < query3.Count(); j++)
+                {
+                    lpoj_submission news2 = query3.ElementAt(j);
+                    if (news2.SUBMISSION_SCORE == 100) ACAnswer++;
+                    AllAnswer++;
+                }
+            }
+            return new KeyValuePair<int, int>(ACAnswer, AllAnswer);
+        }
+
+        public bool Login(string username, string password)
+        {
+            Entity = new lpojEntities();
+            lpoj_users usernow = new lpoj_users();
+            IEnumerable<lpoj_users> query2 = from g in Entity.lpoj_users
+                                             where g.USERS_USERNAME == username && g.USERS_PASSWORD == password
+                                             select g;
+            if (query2.Count() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
